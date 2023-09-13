7c65ac0321a38a4c6ec7699e5f58c43ec0c25897
Bruno Fernandes <brunofernandesf.gd@gmail.com>
2023-09-13 10:33:34 -0300

    Initial commit

---
+++ src/ex02/Calculo.java
@@ -0,0 +1,8 @@
+package ex02;
+
+public class Calculo {
+    public void grades(int geo, int alg, int phy){
+        int avg = (geo + alg + phy) / 3;
+        System.out.println("A média das suas notas é: "+ avg);
+    }
+}
