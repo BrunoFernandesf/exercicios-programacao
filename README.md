0fb078858c40ad1e648c96ae70a79263888a5b72
Bruno Fernandes <brunofernandesf.gd@gmail.com>
2023-10-16 16:36:56 -0300

    ex35 concluído

--- README.md
+++ README.md
@@ -366,7 +366,7 @@ Create a program that:
 * Input: [Mary, John, George, Nicole], [2343, 2134, 5342, 5342]
 * Output: "The average salary is 3790.25$", "Mary's salary is below average","John's salary is below average","George's salary is above average","Nicole's salary is above average"
 
-# ex.35
+# ex.35✔️
 A company asked you to create a program that reads an employee name and salary and store them into proper lists.The number of employees is unknown. To exit the program the user must input "quit". Before exit find the average salary and for each employee print if their salary is above or below average salary. If the employee have above average salary then remove the employee from the list.
 
 Create a program that:
---
+++ src/ex35/AvgSalaries2.java
@@ -0,0 +1,50 @@
+package ex35;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AvgSalaries2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<String> listaNomes = new ArrayList<>();
+        List<Double> listaSal = new ArrayList<>();
+        String nome;
+        double sal;
+        String op = "";
+        double somaSal = 0;
+        int cont = 0;
+
+        while(!op.equals("sair")){
+            System.out.println("Digite o nome");
+            nome = input.next();
+            System.out.println("Digite o salário");
+            sal = input.nextDouble();
+            listaNomes.add(nome);
+            listaSal.add(sal);
+            System.out.println("Deseja continuar? Digite 'sair' para finalizar o programa");
+            op = input.next();
+            cont++;
+            somaSal += sal;
+
+        }
+        double totalSal = somaSal/cont;
+
+        System.out.println("A média dos salários é: ");
+        System.out.printf("%.2f %n", totalSal);
+        System.out.println("---------------------");
+
+        for(int i = 0; i<listaSal.size(); i++){
+            if(listaSal.get(i) < totalSal){
+                System.out.println(listaNomes.get(i) +" é abaixo da média" );
+                listaNomes.remove(i);
+                listaSal.remove(i);
+            }
+            else {
+                System.out.println(listaNomes.get(i) +" é acima da média");
+            }
+            }
+            System.out.println(listaNomes);
+            System.out.println(listaSal);
+        }
+}
