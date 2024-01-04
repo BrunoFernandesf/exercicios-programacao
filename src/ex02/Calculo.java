3a8b23fc5230437831d016f7d166ba286c00d397
BrunoFernandesf <97907031+BrunoFernandesf@users.noreply.github.com>
2024-01-04 19:15:09 -0300

    Delete src directory

--- src/ex02/Calculo.java
+++
@@ -1,9 +0,0 @@
-package ex02;
-
-public class Calculo {
-    public int grades(int geo, int alg, int phy){
-        int avg = (geo + alg + phy) / 3;
-        System.out.println("A média das suas notas é: "+ avg);
-        return avg;
-    }
-}
