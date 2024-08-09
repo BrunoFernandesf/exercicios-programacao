140c81bbf98b94ba63b7beeeb105846c102a380c
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-08-09 11:16:09 -0300

    exercicio 01 cs

---
+++ ExC#/ex-01.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        c = a;
+        a = b;
+        b = c;
+        
+        Console.WriteLine("Var a, b");
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        
+    }
+}
