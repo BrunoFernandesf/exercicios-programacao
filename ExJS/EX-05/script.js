d9b8b0eac83142779492cc730a6867b9ffd24f47
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-08-16 11:04:04 -0300

    EX-05

---
+++ ExJS/EX-05/script.js
@@ -0,0 +1,10 @@
+var width;
+var height;
+var area;
+
+width = Number(prompt("Type the width of the area"));
+height = Number(prompt("Type the height of the area"));
+
+area = width * height;
+
+alert(`The area of the property is ${area}`);
